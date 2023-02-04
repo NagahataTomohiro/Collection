63c64b4da796509f2d6c933d2f39781b34b36ce3
N.Tomohiro <NagahataTomohiro@users.noreply.github.com>
2023-02-04 21:30:30 +0900

    課題提出

--- src/practice/Chapter5.java
+++ src/practice/Chapter5.java
@@ -3,21 +3,28 @@ package practice;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Chapter5 {
+
 	public static void main(String[] args) {
-		List<Task> taskList = new ArrayList<>();
-		taskList.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う。"));
-		taskList.add(new Task(LocalDate.of(2021, 9, 15), "○○社面談。"));
-		taskList.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う。"));
-		taskList.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く。"));
-		taskList.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く。"));
+		List<Task> tasks = new ArrayList<>();
+		tasks.add(new Task(LocalDate.of(2021, 10, 21), "牛乳を買う。"));
+		tasks.add(new Task(LocalDate.of(2021, 9, 15), "○○社面談。"));
+		tasks.add(new Task(LocalDate.of(2021, 12, 4), "手帳を買う。"));
+		tasks.add(new Task(LocalDate.of(2021, 8, 10), "散髪に行く。"));
+		tasks.add(new Task(LocalDate.of(2021, 11, 9), "スクールの課題を解く。"));
 
-		Collections.sort(taskList);
+		Collections.sort(tasks, new Comparator<Task>() {
+			@Override
+			public int compare(Task date, Task content) {
+				return date.getDate().compareTo(content.getDate());
+			}
+		});
 
-		for (Task task : taskList) {
-			System.out.println(task.getDate() + " " + task.getTask());
+		for (Task task : tasks) {
+			System.out.println(task.getDate() + " " + task.getContent());
 		}
 	}
 }
--- src/practice/Task.java
+++ src/practice/Task.java
@@ -2,25 +2,20 @@ package practice;
 
 import java.time.LocalDate;
 
-class Task implements Comparable<Task> {
+public class Task {
 	private LocalDate date;
-	private String task;
+	private String content;
 
-	public Task(LocalDate date, String task) {
+	public Task(LocalDate date, String content) {
 		this.date = date;
-		this.task = task;
+		this.content = content;
 	}
 
 	public LocalDate getDate() {
 		return date;
 	}
 
-	public String getTask() {
-		return task;
-	}
-
-	@Override
-	public int compareTo(Task o) {
-		return this.date.compareTo(o.date);
+	public String getContent() {
+		return content;
 	}
 }
